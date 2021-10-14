2e3717fee88e686cec4fc127546d4cbef374b70c
Casey Harding <casey.harding@generalassemb.ly>
2021-10-14 17:59:46 -0400

    API call in place and working.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=5')
+      .then(res => {
+        console.log(res);
+      }).catch(err => console.error(err));
+  }, [])
   return (
     <div className="App">
       <h1>CaseyBook</h1>
