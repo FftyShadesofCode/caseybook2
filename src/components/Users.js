527d9c2917162a07352051d496a898fdfb51a866
Casey Harding <casey.harding@generalassemb.ly>
2021-10-14 18:34:32 -0400

    User information showing on users page.

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,8 +4,11 @@ const Users = (props) => {
     return (
         <div className="users-wrapper">
             {props.users.map(user => (
-                <div className="user-wrapper">
-                    {user.name}
+                <div className="user-wrapper" key={user.uuid}>
+                    <h2>{user.name}</h2>
+                    <img src={user.picture} alt={user.name} />
+                    <p>Contact: {user.email}</p>
+                    <p>Location: {user.location}</p>
                 </div>
             ))}
         </div>
