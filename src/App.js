81ad35932d8faa6bb8b17e64bfb943108bf3b493
Casey Harding <casey.harding@generalassemb.ly>
2021-10-14 18:05:20 -0400

    Users component scaffolded.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
+/**
+ * user.location -> state
+ * user.uuid
+ * user.picture.medium
+ * user.name -> user.first + user.last
+ * user.posts -> stretch -> likes
+ * user.email
+ */
+
+const admin = {
+  location: "NY",
+  uuid: "12345",
+  picture: "https://wallsdesk.com/wp-content/uploads/2016/12/Thor-High-Quality-Wallpapers.jpg",
+  name: "CRHarding",
+  email: "C@c.com"
+}
+
 function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     axios.get('https://randomuser.me/api/?results=5')
       .then(res => {
-        console.log(res);
+        console.log(res.data.results);
       }).catch(err => console.error(err));
   }, [])
+
   return (
     <div className="App">
       <h1>CaseyBook</h1>
---
+++ src/components/Users.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Users = (props) => {
+    return (
+        <h2>Users Page</h2>
+    )
+}
+
+export default Users;
