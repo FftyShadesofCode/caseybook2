a7068208b14d69705df41a90a2ac7b95ef46b7b1
FrameofLightDesigner <31264591+FrameofLightDesigner@users.noreply.github.com>
2021-10-16 16:45:01 -0400

    Making Progress

--- src/App.js
+++ src/App.js
@@ -8,49 +8,38 @@ import Users from './components/Users';
 import HomePage from './components/HomePage';
 
 const admin = {
-  location: "NY",
-  uuid: "12345",
-  picture: "https://wallsdesk.com/wp-content/uploads/2016/12/Thor-High-Quality-Wallpapers.jpg",
-  name: "CRHarding",
-  email: "C@c.com"
+    location: 'NY',
+    uuid: '1cdcd44d-44cd-43cb-b59f-13d3f4468b5e',
+    picture: 'https://media.gq.com/photos/588913c08d52a3d07dbac878/64:25/w_2469,h_964,c_limit/0217-GQ-FEJO02-01-Prostitution-Therapy-02.jpg',
+    name: 'Imma D. Sucker',
+    email: 'ana.perrin@example.com',
 }
-
 function App() {
-  const [users, setUsers] = useState([admin]);
+    const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://randomuser.me/api/?results=5')
-      .then(res => {
-        console.log(res.data.results);
-      }).catch(err => console.error(err));
-  }, [])
-
-  return (
-    <div className="App">
-      <header>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/users">Friends Page</Link>
-        </nav>
-      </header>
-      <Route path="/users">
-        <Users users={users} />
-      </Route>
-      <Route exact path="/">
-        <HomePage />
-      </Route>
-    </div>
-  );
+    useEffect(() => {
+        axios.get('https://randomuser.me/api/')
+            .then(res => {
+                console.log(res.data.results);
+            }).catch(err => console.error(err));
+    }, [])
+    return (
+        <div className='App'>
+            <header>
+                <nav>
+                    <Link to='/'>Home</Link>
+                    <Link to='/users'>Bitches N' Hoes</Link>
+                    <Link to='/'>DON'T FUCKIN' CLICK ME!</Link>
+                </nav>
+            </header>
+            <Route path='/users'>
+                <Users users={users} />
+            </Route>
+            <Route path='/'>
+                <HomePage />
+            </Route>
+        </div>
+    );
 }
 
-export default App;
-
-
-/**
- * user.location -> state
- * user.uuid
- * user.picture.medium
- * user.name -> user.first + user.last
- * user.posts -> stretch -> likes
- * user.email
- */
+export default App
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,8 +3,8 @@ import React from 'react';
 const HomePage = () => {
     return (
     <div className="home-wrapper">
-        <h1>CaseyBook</h1>
-        <h2>It's like facebook, only made by some guy called Casey</h2>
+        <h1>BitchBook</h1>
+        <h2>BLAM! It's like facebook, but it's for my bitches n' hoes!</h2>
     </div>
     )
 }
